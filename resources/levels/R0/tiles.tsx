50920f1921e484abf2545cf4565563a296dfa5ac
Lucas S. Vieira <lucasvieira@protonmail.com>
2023-08-26 17:01:59 -0300

    Add R0 initial layout

---
+++ resources/levels/R0/tiles.tsx
@@ -0,0 +1,142 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="R12" tilewidth="128" tileheight="128" tilecount="16" columns="4">
+ <image source="chunks.png" width="512" height="512"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="128" height="128"/>
+  </objectgroup>
+ </tile>
+ <tile id="1">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0">
+    <polygon points="0,0 128,128 0,128"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="2">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="128" y="0">
+    <polygon points="0,0 -128,128 0,128"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="3">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="128" y="128">
+    <polygon points="0,0 -128,-128 0,-128"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="4">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="128">
+    <polygon points="0,0 128,-128 0,-128"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="5">
+  <objectgroup draworder="index" id="3">
+   <object id="8" x="0" y="128">
+    <polygon points="0,0 0,-128 6.66667,-107"/>
+   </object>
+   <object id="9" x="0" y="128">
+    <polygon points="0,0 6.63636,-107.545 18.5,-80.5"/>
+   </object>
+   <object id="12" x="0" y="128">
+    <polygon points="0,0 18.5,-80.25 30.125,-61.875"/>
+   </object>
+   <object id="13" x="0" y="128">
+    <polygon points="0,0 30.1875,-61.75 45.3125,-44.5"/>
+   </object>
+   <object id="14" x="0" y="128">
+    <polygon points="0,0 45.4545,-44.2727 65.1818,-28.2727"/>
+   </object>
+   <object id="15" x="0" y="128">
+    <polygon points="0,0 65,-28.1818 83,-17.0909"/>
+   </object>
+   <object id="16" x="0" y="128">
+    <polygon points="0,0 83,-17 128,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="6">
+  <objectgroup draworder="index" id="2">
+   <object id="2" x="128" y="128">
+    <polygon points="0,0 0,-128 -6.66667,-107"/>
+   </object>
+   <object id="3" x="128" y="128">
+    <polygon points="0,0 -6.63636,-107.545 -18.5,-80.5"/>
+   </object>
+   <object id="4" x="128" y="128">
+    <polygon points="0,0 -18.5,-80.25 -30.125,-61.875"/>
+   </object>
+   <object id="5" x="128" y="128">
+    <polygon points="0,0 -30.1875,-61.75 -45.3125,-44.5"/>
+   </object>
+   <object id="6" x="128" y="128">
+    <polygon points="0,0 -45.4545,-44.2727 -65.1818,-28.2727"/>
+   </object>
+   <object id="7" x="128" y="128">
+    <polygon points="0,0 -65,-28.1818 -83,-17.0909"/>
+   </object>
+   <object id="8" x="128" y="128">
+    <polygon points="0,0 -83,-17 -128,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="7">
+  <objectgroup draworder="index" id="2">
+   <object id="2" x="128" y="0">
+    <polygon points="0,0 0,128 -6.66667,107"/>
+   </object>
+   <object id="3" x="128" y="0">
+    <polygon points="0,0 -6.63636,107.545 -18.5,80.5"/>
+   </object>
+   <object id="4" x="128" y="0">
+    <polygon points="0,0 -18.5,80.25 -30.125,61.875"/>
+   </object>
+   <object id="5" x="128" y="0">
+    <polygon points="0,0 -30.1875,61.75 -45.3125,44.5"/>
+   </object>
+   <object id="6" x="128" y="0">
+    <polygon points="0,0 -45.4545,44.2727 -65.1818,28.2727"/>
+   </object>
+   <object id="7" x="128" y="0">
+    <polygon points="0,0 -65,28.1818 -83,17.0909"/>
+   </object>
+   <object id="8" x="128" y="0">
+    <polygon points="0,0 -83,17 -128,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="8">
+  <objectgroup draworder="index" id="2">
+   <object id="2" x="0" y="0">
+    <polygon points="0,0 0,128 6.66667,107"/>
+   </object>
+   <object id="3" x="0" y="0">
+    <polygon points="0,0 6.63636,107.545 18.5,80.5"/>
+   </object>
+   <object id="4" x="0" y="0">
+    <polygon points="0,0 18.5,80.25 30.125,61.875"/>
+   </object>
+   <object id="5" x="0" y="0">
+    <polygon points="0,0 30.1875,61.75 45.3125,44.5"/>
+   </object>
+   <object id="6" x="0" y="0">
+    <polygon points="0,0 45.4545,44.2727 65.1818,28.2727"/>
+   </object>
+   <object id="7" x="0" y="0">
+    <polygon points="0,0 65,28.1818 83,17.0909"/>
+   </object>
+   <object id="8" x="0" y="0">
+    <polygon points="0,0 83,17 128,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="9">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="64" width="128" height="64"/>
+  </objectgroup>
+ </tile>
+</tileset>
