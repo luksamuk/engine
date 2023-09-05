646d671ff4fc1e61ddd70d34dc82648c20482734
Lucas S. Vieira <lucasvieira@protonmail.com>
2023-09-05 12:33:28 -0300

    Add R3 assets

---
+++ resources/levels/R3/tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="tiles" tilewidth="128" tileheight="128" tilecount="72" columns="9">
+ <image source="chunks.png" width="1152" height="1024"/>
+</tileset>
