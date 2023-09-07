453a22f8b5a367eacadf0e646a7d1d87340ab6dc
Lucas S. Vieira <lucasvieira@protonmail.com>
2023-09-07 20:03:12 -0300

    Adjustments to graphics on R2 (now renamed Speed Slider)

--- resources/levels/R2/tiles.tsx
+++ resources/levels/R2/tiles.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="R5" tilewidth="128" tileheight="128" tilecount="256" columns="16">
- <image source="chunks.png" trans="000088" width="2048" height="2048"/>
+<tileset version="1.10" tiledversion="1.10.2" name="R5" tilewidth="128" tileheight="128" tilecount="243" columns="9">
+ <image source="chunks.png" trans="000088" width="1152" height="3456"/>
 </tileset>
