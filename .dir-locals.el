95d362846f4ae8952bc94f252ed9026bb7478b9a
Lucas S. Vieira <lucasvieira@protonmail.com>
2023-09-19 11:54:37 -0300

    Add Flecs and entity test

--- .dir-locals.el
+++ .dir-locals.el
@@ -3,8 +3,10 @@
             (setq-local flycheck-clang-include-path
                         (list (concat root "include")
 			      (concat root "imgui-1.89.9")
-			      (concat root "imgui-1.89.9/backends")))
+			      (concat root "imgui-1.89.9/backends")
+			      (concat root "flecs-3.2.7")))
 	    (setq-local flycheck-gcc-include-path
 			(list (concat root "include")
 			      (concat root "imgui-1.89.9")
-			      (concat root "imgui-1.89.9/backends")))))))
+			      (concat root "imgui-1.89.9/backends")
+			      (concat root "flecs-3.2.7")))))))
