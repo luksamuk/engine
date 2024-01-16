6c5e6e778785072ecba7ab1d8cdb42a402922652
Lucas S. Vieira <lucasvieira@protonmail.com>
2024-01-16 20:40:50 -0300

    Add Eggmanland

---
+++ resources/levels/R9/tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="R5" tilewidth="128" tileheight="128" tilecount="243" columns="9">
+ <image source="chunks.png" trans="000088" width="1152" height="3456"/>
+</tileset>
